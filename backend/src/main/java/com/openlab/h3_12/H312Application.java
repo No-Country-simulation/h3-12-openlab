361caf3e1d14a8162fa8cc434fcf1f5135dc2093
MiguelAChavez <chavez.miguel.angel154@gmail.com>
2024-11-13 19:26:24 -0300

    feat: initialize backend project structure

---
+++ backend/src/main/java/com/openlab/h3_12/H312Application.java
@@ -0,0 +1,13 @@
+package com.openlab.h3_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H312Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/openlab/h3_12/H312ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openlab.h3_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H312ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
