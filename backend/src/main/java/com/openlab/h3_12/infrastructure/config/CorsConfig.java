8385e40101eede65c02b57c162672d1c453b5406
MiguelAChavez <chavez.miguel.angel154@gmail.com>
2024-11-19 08:23:43 -0300

    [BACK] fix: Add missing AudienceValidator and CorsConfig files

---
+++ backend/src/main/java/com/openlab/h3_12/infrastructure/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.openlab.h3_12.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+public class CorsConfig {
+    @Value("${web.cors.allowed-origins}")
+    private static String corsAllowedOrigins;
+
+    @Bean
+    public static CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(corsAllowedOrigins));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
