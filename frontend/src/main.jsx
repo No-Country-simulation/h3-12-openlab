baee4d17330a2b8abdcad9e4ade774fc35cc4ce5
MiguelAChavez <chavez.miguel.angel154@gmail.com>
2024-12-06 19:42:27 -0300

    [FRONT] config: add variable environments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain="dev-c4drekbp1tnxy150.us.auth0.com"
-      clientId="Hjqt9vRBLPuTGZ7ORDI6CUZqy698TQWs"
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
       }}
       cacheLocation="localstorage" // Store tokens in localStorage
       useRefreshTokens={true} // Enable refresh tokens for longer sessions
